bbd2fcc35196f772bbd75e4fb5ddd3dc2811f0fc
Evgeniy Kubyshin <ekubyshin@gmail.com>
2016-12-29 22:07:44 +0300

    fixed

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,24 +1,24 @@
 package controllers
 
-import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase, ScalaObservable}
 import play.api._
 import play.api.libs.json.Json
 import play.api.mvc._
+
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
+import scala.concurrent.duration._
 
 /**
   * Created by Karpychev on 29.12.2016.
   */
 object Users extends Controller {
-  def index = Action {
-    val mongoClient: MongoClient = MongoClient("mongodb://corp.pharm.local:27017")
+  def index = Action.async {
+    val mongoClient: MongoClient = MongoClient("mongodb://127.0.0.1:27017/")
     val database: MongoDatabase = mongoClient.getDatabase("portal")
     val collection: MongoCollection[Document] = database.getCollection("users")
-    val results = collection.find().limit(30).collect()
-    val json = Json.obj(
-      "name" -> "Maksim",
-      "age" -> "30",
-      "created" -> new java.util.Date().getTime()
-    )
-    Ok(json)
+    collection.find().collect().subscribe((results: Seq[Document]) => println(s"Found BsonDocuments: #${results.size}"))
+    collection.find().collect().toFuture().map(seq => Ok(Document("users" -> seq).toJson))
   }
 }
